af91106802d70f2c2aef3c47bf3483b8e83153e0
thaindcode <thai8442444@gmail.com>
2022-06-03 14:46:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ export function randomBytes (length, cb) {
     return new Buffer(hexString, 'hex')
   }
 
-  RNRandomBytes?.randomBytes(length, function(err, base64String) {
+  RNRandomBytes.randomBytes(length, function(err, base64String) {
     if (err) {
       cb(err)
     } else {
